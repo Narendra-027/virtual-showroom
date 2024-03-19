466104c264a16023c70320a00da2b99030dbb5fe
Narendra Jani <89453812+Narendra-027@users.noreply.github.com>
2024-03-20 02:46:29 +0530

    react Link updated

--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
 import { useSelector } from "react-redux";
 
@@ -28,18 +29,18 @@ function LeftMenu(props) {
       return (
         <Menu mode={props.mode}>
         <Menu.Item key="mail">
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </Menu.Item>
         <SubMenu title={<span>About Us</span>}>
           <MenuItemGroup title="Who are we ?">
-            <Menu.Item key="setting:1"><a href="/team">Team</a></Menu.Item>
-            <Menu.Item key="setting:2"><a href="/service">Our Services</a></Menu.Item>
-            <Menu.Item key="setting:3"><a href="/reviews">Reviews</a></Menu.Item>
-            <Menu.Item key="setting:4"><a href="/contacts">Contact Us</a></Menu.Item>
+            <Menu.Item key="setting:1"><Link to="/team">Team</Link></Menu.Item>
+            <Menu.Item key="setting:2"><Link to="/service">Our Services</Link></Menu.Item>
+            <Menu.Item key="setting:3"><Link to="/reviews">Reviews</Link></Menu.Item>
+            <Menu.Item key="setting:4"><Link to="/contacts">Contact Us</Link></Menu.Item>
           </MenuItemGroup>
         </SubMenu>
         <Menu.Item key="setting:5">
-          <a href="/orderlist">Orders</a>
+          <Link to="/orderlist">Orders</Link>
         </Menu.Item>
       </Menu>
       )
@@ -47,14 +48,14 @@ function LeftMenu(props) {
       return (
         <Menu mode={props.mode}>
         <Menu.Item key="mail">
-          <a href="/">Home</a>
+          <Link to="/">Home</Link>
         </Menu.Item>
         <SubMenu title={<span>About Us</span>}>
           <MenuItemGroup title="Who are we ?">
-            <Menu.Item key="setting:1"><a href="/team">Team</a></Menu.Item>
-            <Menu.Item key="setting:2"><a href="/service">Our Services</a></Menu.Item>
-            <Menu.Item key="setting:3"><a href="/reviews">Reviews</a></Menu.Item>
-            <Menu.Item key="setting:4"><a href="/contacts">Contact Us</a></Menu.Item>
+            <Menu.Item key="setting:1"><Link to="/team">Team</Link></Menu.Item>
+            <Menu.Item key="setting:2"><Link to="/service">Our Services</Link></Menu.Item>
+            <Menu.Item key="setting:3"><Link to="/reviews">Reviews</Link></Menu.Item>
+            <Menu.Item key="setting:4"><Link to="/contacts">Contact Us</Link></Menu.Item>
           </MenuItemGroup>
         </SubMenu>
       </Menu>
